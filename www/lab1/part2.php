a91b8be8ada2f2474de8fdbeb190f2e88a3bebc8
Leonards Tesnovs <31099868+limzikiki@users.noreply.github.com>
2022-03-08 18:11:44 +0200

    Update part2.php

--- www/lab1/part2.php
+++ www/lab1/part2.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-header("Content-Type: text/html; charset=UTF-8");
+header("Content-Type: text/plain; charset=UTF-8");
 
 
 
@@ -15,31 +15,29 @@ for($i = 1; $i <= 9; $i++){
     for($j = 9; $j >= $i; $j--){
         _print($j);
     }
-    _print("<br/>");
+    _print("\n");
 }
 
 function easter($year){
-    $g = $year % 19;
-    $c = $year / 100;
-    $h = ($c - intval($c / 4) -
-            intval((8 * $c + 13) / 25) + 19 * $g + 15) % 30;
-    $i = $h - intval($h / 28) * (1 - intval($h / 28) *
-            intval(29 / ($h + 1)) * intval((21 - $g) / 11));
-
-    $day = $i - (($year + intval($year / 4) +
-                $i + 2 - $c + intval($c / 4)) % 7) + 28;
-    $month = 3;
-
-    if ($day > 31)
-    {
-        $month++;
-        $day -= 31;
-    }
-    return mktime(0, 0, 0, $month, $day, $year,);
+    $a = $year % 19;
+    $b = floor($year / 100);
+    $c = $year % 100;
+    $d = floor($b / 4);
+    $e = $b % 4;
+    $f = floor(($b+8) / 25);
+    $g = floor(($b-$f+1) / 3);
+    $h = (19*$a+$b-$d-$g+15) % 30;
+    $i = floor($c / 4);
+    $k = $c % 4;
+    $l = (32+2*$e+2*$i-$h-$k) % 7;
+    $m = floor(($a+11*$h+22*$l) / 451);
+    $n = floor(($h+$l-7*$m+114) / 31);
+    $p = floor(($h+$l-7*$m+114) % 31);
+    return mktime(0, 0, 0, $n, $p+1, $year,);
 }
 
 // easter test
 for($i = 0; $i < 10; $i++){
     $date = easter(2022 + $i);
-    _print(date('\E\a\s\t\e\r \i\n Y, F j', $date) . "<br/>");
+    _print(date('\E\a\s\t\e\r \i\n Y, F j', $date) . "\n");
 }
